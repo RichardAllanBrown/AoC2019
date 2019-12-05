e8dabcc19b952fe5b6f4db8488060e6ab057121a
Richard Brown <richard.brown@67bricks.com>
2019-12-05 13:05:39 +0000

    Completed day 3 part 1

---
+++ AdventOfCode.App/Day3.fs
@@ -0,0 +1,95 @@
+﻿module Day3
+
+type Coordinate = { x: int; y: int }
+
+let origin = { x = 0; y = 0 }
+
+type Direction =
+    | Up
+    | Right
+    | Down
+    | Left
+
+type WireSegment = { origin: Coordinate; direction: Direction; distance: int }
+
+let calculateDestination(w: WireSegment): Coordinate =
+    match w.direction with
+    | Up -> { x = w.origin.x; y = w.origin.y + w.distance }
+    | Right -> { x = w.origin.x + w.distance; y = w.origin.y }
+    | Down -> { x = w.origin.x; y = w.origin.y - w.distance }
+    | Left -> { x = w.origin.x - w.distance; y = w.origin.y }
+
+let isVertical(d: Direction): bool =
+    match d with
+    | Up -> true
+    | Down -> true
+    | _ -> false
+
+let isHorizontal(d: Direction): bool = isVertical(d) |> not
+
+let rec between(oneEnd: int, otherEnd: int)(value: int): bool =
+    if (oneEnd <= otherEnd) then oneEnd <= value && value <= otherEnd
+    else between(otherEnd, oneEnd)(value)
+
+let hasCrossingPoint(vertical: WireSegment, horizontal: WireSegment): bool =
+    let xAxisWithinBounds = between(horizontal.origin.x, calculateDestination(horizontal).x)(vertical.origin.x)
+    let yAxisWithinBounds = between(vertical.origin.y, calculateDestination(vertical).y)(horizontal.origin.y)
+    xAxisWithinBounds && yAxisWithinBounds
+
+let calculateCrossingPoint(first: WireSegment, second: WireSegment): Coordinate option =
+    let verticalWire = [first; second] |> List.tryFind(fun w -> isVertical(w.direction))
+    let horizontalWire = [first; second] |> List.tryFind(fun w -> isHorizontal(w.direction))
+
+    match (verticalWire, horizontalWire) with
+    | (Some(vert), Some(hor)) when vert <> hor && hasCrossingPoint(vert, hor) -> Some({ x = vert.origin.x; y = hor.origin.y })
+    | _ -> None
+
+let toList =
+    function
+    | Some a -> a :: []
+    | None -> []
+
+let calculateAllCrossingPoints(firstWire: WireSegment list, secondWire: WireSegment list): Coordinate list =
+    let crossingPointOpts = [
+        for firstSeg in firstWire do
+        for otherSeg in secondWire do
+            calculateCrossingPoint(firstSeg, otherSeg)
+    ]
+    crossingPointOpts |> List.collect(toList)
+    
+let manhattenDistanceFromOrigin(coord: Coordinate): int = abs(coord.x) + abs(coord.y)
+
+let parseDirection(c: char): Direction option =
+    match c with
+    | 'U' -> Some(Up)
+    | 'R' -> Some(Right)
+    | 'D' -> Some(Down)
+    | 'L' -> Some(Left)
+    | _ -> None
+
+let parseIt(previousSegments: WireSegment list)(raw: string) : WireSegment list =
+    let direction = parseDirection(raw.Chars(0)).Value
+    let source = previousSegments |> List.tryLast |> Option.map(calculateDestination) |> Option.defaultValue(origin)
+    let distance = raw.Substring(1) |> int
+    let newSegment = { origin = source; direction = direction; distance = distance }
+    List.append(previousSegments)([newSegment])
+
+let buildWireSegments(source: string): WireSegment list =
+    source
+    |> Utils.splitLine
+    |> List.fold(parseIt)([])
+
+let calculateClosestCrossingPointDistance(oneWire: WireSegment list, otherWire: WireSegment list) : int = 
+    calculateAllCrossingPoints(oneWire, otherWire)
+    |> List.filter(fun c -> c <> origin)
+    |> List.map(manhattenDistanceFromOrigin)
+    |> List.min
+
+let computeDay3Part1Answer(): int =
+    let wires = Utils.readInputFile("./input/Day3.txt") |> List.map(buildWireSegments)
+    calculateClosestCrossingPointDistance(wires.Item(0), wires.Item(1))
+
+let computeDay3Part2Answer(): int =
+    // calculate all crossingpoints less origin with wires that the crossing happened at
+    // compute wires 
+    0
---
+++ AdventOfCode.Tests/Day3Tests.fs
@@ -0,0 +1,66 @@
+﻿module Day3Tests
+
+open Day3;
+open Xunit;
+
+type ClassDataBase(generator : obj [] seq) = 
+    interface seq<obj []> with
+        member this.GetEnumerator() = generator.GetEnumerator()
+        member this.GetEnumerator() = 
+            generator.GetEnumerator() :> System.Collections.IEnumerator
+
+type CalculateDestinationTestData() = 
+    inherit ClassDataBase([
+        [| { origin = { x = 0; y = 0 }; direction = Up; distance = 1 }; { x = 0; y = 1 } |];
+        [| { origin = { x = 100; y = 20 }; direction = Right; distance = 23 }; { x = 123; y = 20 } |];
+        [| { origin = { x = 1000; y = 450 }; direction = Down; distance = 12 }; { x = 1000; y = 438 } |];
+        [| { origin = { x = 760; y = 340 }; direction = Left; distance = 32 }; { x = 728; y = 340 } |]
+    ])
+
+[<Theory; ClassData(typeof<CalculateDestinationTestData>)>]
+let ``Calculate destination from origin returns expected``(wire: WireSegment, dest: Coordinate) =
+    Assert.Equal(dest, calculateDestination(wire))
+
+[<Theory>]
+[<InlineData(4, 8, 5, true)>]
+[<InlineData(8, 4, 5, true)>]
+[<InlineData(4, 8, 1, false)>]
+[<InlineData(1, 2, 1, true)>]
+[<InlineData(1, 2, 5, false)>]
+let ``Between calulcates expected result``(oneEnd: int, otherEnd: int, value: int, expected: bool) =
+    Assert.Equal(expected, between(oneEnd, otherEnd)(value))
+
+type CalulateCrossingPointTestData() = 
+    inherit ClassDataBase([
+        [| { origin = { x = 1; y = 1 }; direction = Up; distance = 5 };
+            { origin = { x = 0; y = 4 }; direction = Right; distance = 12 };
+            { x = 1; y = 4 } |];
+        [| { origin = { x = 0; y = 4 }; direction = Right; distance = 12 };
+            { origin = { x = 1; y = 1 }; direction = Up; distance = 5 };            
+            { x = 1; y = 4 } |];
+        [| { origin = { x = 0; y = 0 }; direction = Down; distance = 12 };
+            { origin = { x = 0; y = 0 }; direction = Left; distance = 5 };            
+            { x = 0; y = 0 } |];
+        [| { origin = { x = 0; y = 0 }; direction = Right; distance = 51 };
+            { origin = { x = 4; y = 12 }; direction = Down; distance = 35 };            
+            { x = 4; y = 0 } |]
+    ])
+
+[<Theory; ClassData(typeof<CalulateCrossingPointTestData>)>]
+let ``Crossing coordinate can be calculated``(oneWire: WireSegment, otherWire: WireSegment, expectedCrossing: Coordinate) =
+    Assert.Equal(Some(expectedCrossing), calculateCrossingPoint(oneWire, otherWire))
+
+[<Fact>]
+let ``Works for simple test case``() =
+    let oneWire = buildWireSegments("R75,D30,R83,U83,L12,D49,R71,U7,L72")
+    let otherWire = buildWireSegments("U62,R66,U55,R34,D71,R55,D58,R83")
+    Assert.Equal(159, calculateClosestCrossingPointDistance(oneWire, otherWire))
+
+[<Fact>]
+let ``Works for a slightly more complex test case``() =
+    let oneWire = buildWireSegments("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+    let otherWire = buildWireSegments("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+    Assert.Equal(135, calculateClosestCrossingPointDistance(oneWire, otherWire))
+
+[<Fact>]
+let ``Calculates correct answer for part 1 input``() = Assert.Equal(1064, computeDay3Part1Answer())
